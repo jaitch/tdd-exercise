aa7bc6b42ec91cba37cb34e80402cf2e69c6a94e
Janice Huang <janicehuang@gmail.com>
2019-08-13 20:43:06 -0700

    write tests for number cards and face cards (not ace)

--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -10,23 +10,30 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Blackjack Score' do
   it 'can calculate the score for a pair of number cards' do
-
     # Arrange
     hand = [3, 4]
-
     # Act
     score = blackjack_score(hand)
-
     # Assert <-  You do this part!
-
+    expect(score).must_equal 7
   end
 
   it 'facecards have values calculated correctly' do
-
+    # Arrange
+    hand = ['Jack', 'Queen']
+    #Act
+    score = blackjack_score(hand)
+    # Assert
+    expect(score).must_equal 20
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
-
+    # Arrange
+    hand = ['Ace', Queen, 5]
+    # Act
+    score = blackjack_score(hand)
+    # Assert
+    expect(score).must_equal 16
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
